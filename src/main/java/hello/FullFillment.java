5e53764d5ac711b0f05b900ea6db5da7fe9d22a3
14Sakshi <sakshiapiai@gmail.com>
2017-06-27 13:40:08 -0400

    Add files via upload

---
+++ src/main/java/hello/FullFillment.java
@@ -0,0 +1,27 @@
+package hello;
+
+public class FullFillment {
+	String speech;
+	String source;
+	String displayText;
+	public String getSpeech() {
+		return speech;
+	}
+	public void setSpeech(String speech) {
+		this.speech = speech;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public String getDisplayText() {
+		return displayText;
+	}
+	public void setDisplayText(String displayText) {
+		this.displayText = displayText;
+	}
+	
+
+}
