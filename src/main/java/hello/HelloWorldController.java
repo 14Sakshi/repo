3fe9beda33701308ea1cd25a5fa1a940acedd3d8
SruSak <apiaiSak@gmail.com>
2017-06-27 08:40:49 -0400

    Add files via upload

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -1,10 +1,14 @@
 package hello;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+
 
 @Controller
 @RequestMapping("/webhook")
@@ -14,7 +18,7 @@ public class HelloWorldController {
     public @ResponseBody WebhookResponse webhook(@RequestBody String obj){
 
         System.out.println(obj);
-
-        return new WebhookResponse("Hello! " + obj, "Text " + obj);
-    }
+        Date d=new Date();
+        return new WebhookResponse("Hello! " + d, "Text " + obj);
+    }//webhook
 }
